88f9913b317bae76011e94e4e8787e21dd18cee0
rozz12 <51398868+rozz12@users.noreply.github.com>
2021-07-08 16:05:23 +0545

    added review functionality

--- Project-Web/productDetails.php
+++ Project-Web/productDetails.php
@@ -394,40 +394,43 @@
             <p class="fs-5 text-center">
               <strong>Your rating</strong>
             </p>
-            <div class="form-check mb-4">
-              <input class="form-check-input" name="group1" type="radio" id="radio-179" value="option1" checked>
-              <label class="form-check-label" for="radio-179">Very good</label>
-            </div>
+            <form action="php/review.php" method="POST">
+              <input type="hidden" name="prod_id" value="<?php echo $product_id?>">
+              <div class="form-check mb-4">
+                <input class="form-check-input" name="rating" type="radio" id="radio-179" value="1" checked>
+                <label class="form-check-label" for="radio-179">Very good</label>
+              </div>
 
-            <div class="form-check mb-4">
-              <input class="form-check-input" name="group1" type="radio" id="radio-279" value="option2">
-              <label class="form-check-label" for="radio-279">Good</label>
-            </div>
+              <div class="form-check mb-4">
+                <input class="form-check-input" name="rating" type="radio" id="radio-279" value="2">
+                <label class="form-check-label" for="radio-279">Good</label>
+              </div>
 
-            <div class="form-check mb-4">
-              <input class="form-check-input" name="group1" type="radio" id="radio-379" value="option3">
-              <label class="form-check-label" for="radio-379">Mediocre</label>
-            </div>
-            <div class="form-check mb-4">
-              <input class="form-check-input" name="group1" type="radio" id="radio-479" value="option4">
-              <label class="form-check-label" for="radio-479">Bad</label>
-            </div>
-            <div class="form-check mb-4">
-              <input class="form-check-input" name="group1" type="radio" id="radio-579" value="option5">
-              <label class="form-check-label" for="radio-579">Very bad</label>
-            </div>
+              <div class="form-check mb-4">
+                <input class="form-check-input" name="rating" type="radio" id="radio-379" value="3">
+                <label class="form-check-label" for="radio-379">Mediocre</label>
+              </div>
+              <div class="form-check mb-4">
+                <input class="form-check-input" name="rating" type="radio" id="radio-479" value="4">
+                <label class="form-check-label" for="radio-479">Bad</label>
+              </div>
+              <div class="form-check mb-4">
+                <input class="form-check-input" name="rating" type="radio" id="radio-579" value="5">
+                <label class="form-check-label" for="radio-579">Very bad</label>
+              </div>
 
-            <p class="fs-5 text-center">
-              <strong>What could we improve?</strong>
-            </p>
-            <!--textarea-->
-            <div class="md-form">
-              <textarea type="text" id="form79textarea" class="md-textarea form-control" rows="3"></textarea>
-              <label for="form79textarea">Your message</label>
-            </div>
+              <p class="fs-5 text-center">
+                <strong>What could we improve?</strong>
+              </p>
+              <!--textarea-->
+              <div class="md-form">
+                <textarea type="text" id="form79textarea" class="md-textarea form-control" name="msg" rows="3"></textarea>
+                <label for="form79textarea">Your message</label>
+              </div>
+          </form>
           </div>
           <div class="modal-footer d-flex justify-content-md-end edit_productbuttons">
-            <a type="button" class="btn btn-outline-danger waves-effect waves-light">Send
+            <a type="submit" name="send" class="btn btn-outline-danger waves-effect waves-light">Send
               <i class="fa fa-paper-plane ml-1"></i>
             </a>
             <a type="button" class="btn btn-outline-primary waves-effect" data-dismiss="modal">Cancel</a>
@@ -435,7 +438,6 @@
         </div>
       </div>
     </div>
-  </div>
 
     <!--Footer-->
     <?php
