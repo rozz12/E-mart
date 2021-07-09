16f51b0a017aa69b48d3ecc8fe20f072b700d595
rozz12 <51398868+rozz12@users.noreply.github.com>
2021-07-10 01:14:11 +0545

    Delete Project-Web directory

--- Project-Web/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('connection.php');
-	session_start();
-	if (!isset($_SESSION['userid'])) {
-		header('Location: ../Homepage.php');
-	}
-	session_destroy();
-	header('Location: ../Homepage.php');
-?>
